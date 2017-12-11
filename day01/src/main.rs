4f45ed286df4de7a86764f435ec06e5acd33462f
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-12-11 17:07:06 +0100

    Update day01 to read file at runtime instead of compile time

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,7 +1,10 @@
 extern crate day01;
 use day01::captcha_str;
 
+extern crate util;
+use util::read_file;
+
 fn main() {
-    let input_str = include_str!("../input.txt").trim();
-    println!("Captcha for input: {:?}", captcha_str(&input_str));
+    let input = read_file("../input.txt");
+    println!("Captcha for input: {:?}", captcha_str(input.trim()));
 }
