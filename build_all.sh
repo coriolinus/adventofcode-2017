2d4eb525bdab5408a11bea9bdd95821522110a5a
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-12-11 17:06:04 +0100

    Quiet build_all

--- build_all.sh
+++ build_all.sh
@@ -5,7 +5,7 @@ for dir in *; do
    if [ -d $dir -a -f "$dir/Cargo.toml" -a -f "$dir/src/lib.rs" ]; then
       (
          cd $dir
-         cargo update
+         cargo update --quiet
          cargo check --quiet
          if [ $? != 0 ]; then
             echo "Build failed for $dir"
