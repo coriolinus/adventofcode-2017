4528bc2d17bbadb26f9ee0ead6b85e5f91f8a165
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-12-13 15:25:03 +0100

    Fix input path

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -5,6 +5,6 @@ extern crate util;
 use util::read_file;
 
 fn main() {
-    let input = read_file("../input.txt");
+    let input = read_file("input.txt");
     println!("Captcha for input: {:?}", captcha_str(input.trim()));
 }
