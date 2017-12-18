d6767c0ee50f71b438828260cdfb58749107ccef
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2017-12-18 13:54:38 +0100

    Solve part 2.
    This removes part 1, but it's a more interesting problem anyway.
    See previous commit message for details.
    
    Pretty happy with the results here:
    
    $ cargo build --release && time target/release/day18
        Finished release [optimized] target(s) in 0.1 secs
    Machine 1 send count: 7366
    
    real    0m0.063s
    user    0m0.016s
    sys     0m0.016s
    
    The speed of execution wasn't such a big deal, though; we weren't
    asked to execute particularly complex programs. What was really
    interesting and fun was simulating multithreaded execution; it was
    just a fun problem to think about.

--- day18/src/lib.rs
+++ day18/src/lib.rs
@@ -1,10 +1,13 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
+use std::rc::Rc;
+use std::sync::RwLock;
 
 pub mod parser;
 
 pub type RegisterID = char;
 pub type RegisterValue = i64;
 type Registers = HashMap<RegisterID, RegisterValue>;
+type Queue = Rc<RwLock<VecDeque<RegisterValue>>>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ValueHolder {
@@ -33,37 +36,57 @@ pub enum Instruction {
     Jgz(ValueHolder, ValueHolder),
 }
 
-pub struct Machine<'a> {
-    instructions: &'a [Instruction],
-    instruction_pointer: RegisterValue,
-    registers: Registers,
-    sound_playing: Option<RegisterValue>,
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum MachineError {
+    IPOutOfBounds,
+    ReadBlock,
 }
 
-impl<'a> Machine<'a> {
-    pub fn new(instructions: &'a [Instruction]) -> Machine<'a> {
+pub struct Machine<'instructions> {
+    instructions: &'instructions [Instruction],
+    instruction_pointer: RegisterValue,
+    registers: Registers,
+    send_q: Queue,
+    recv_q: Queue,
+}
+
+impl<'instructions> Machine<'instructions> {
+    pub fn new(
+        instructions: &'instructions [Instruction],
+        id: RegisterValue,
+        send_q: Queue,
+        recv_q: Queue,
+    ) -> Machine<'instructions> {
         Machine {
             instructions: instructions,
             instruction_pointer: 0,
-            registers: HashMap::new(),
-            sound_playing: None,
+            registers: {
+                let mut h = HashMap::new();
+                h.insert('p', id);
+                h
+            },
+            send_q: send_q,
+            recv_q: recv_q,
         }
     }
 
     /// Execute a single instruction
-    fn execute(&mut self) -> Result<(Instruction, RegisterValue), ()> {
+    fn execute(&mut self) -> Result<(Instruction, RegisterValue), MachineError> {
         if !(self.instruction_pointer >= 0 &&
                  (self.instruction_pointer as usize) < self.instructions.len())
         {
-            return Err(());
+            return Err(MachineError::IPOutOfBounds);
         }
         let (ip, rv) = {
             use Instruction::*;
             match self.instructions[self.instruction_pointer as usize] {
-                Snd(soundh) => {
-                    let sound = soundh.value(&mut self.registers);
-                    self.sound_playing = Some(sound);
-                    (self.instruction_pointer + 1, (Snd(soundh), sound))
+                Snd(messageh) => {
+                    let message = messageh.value(&mut self.registers);
+                    self.send_q
+                        .write()
+                        .expect("couldn't get send lock")
+                        .push_back(message);
+                    (self.instruction_pointer + 1, (Snd(messageh), message))
                 }
                 Set(id, valueh) => {
                     let value = valueh.value(&mut self.registers);
@@ -86,11 +109,15 @@ impl<'a> Machine<'a> {
                     (self.instruction_pointer + 1, (Mod(id, valueh), value))
                 }
                 Rcv(id) => {
-                    let sound = self.sound_playing.unwrap_or(0);
-                    if *self.registers.entry(id).or_insert(0) != 0 {
-                        (self.instruction_pointer + 1, (Rcv(id), sound))
+                    if let Some(message) = self.recv_q
+                        .write()
+                        .expect("couldn't get receive lock")
+                        .pop_front()
+                    {
+                        self.registers.insert(id, message);
+                        (self.instruction_pointer + 1, (Rcv(id), message))
                     } else {
-                        (self.instruction_pointer + 1, (Rcv(id), 0))
+                        return Err(MachineError::ReadBlock);
                     }
                 }
                 Jgz(x, y) => {
@@ -115,5 +142,39 @@ impl<'a> Machine<'a> {
             }
         }
         None
+    }
+}
+
+pub struct Duet<'instructions> {
+    machine_0: Machine<'instructions>,
+    machine_1: Machine<'instructions>,
+    send_count_1: usize,
+}
+
+impl<'instructions> Duet<'instructions> {
+    pub fn new(instructions: &'instructions [Instruction]) -> Duet {
+        let queue_0 = Rc::new(RwLock::new(VecDeque::new()));
+        let queue_1 = Rc::new(RwLock::new(VecDeque::new()));
+        let machine_0 = Machine::new(instructions, 0, queue_1.clone(), queue_0.clone());
+        let machine_1 = Machine::new(instructions, 1, queue_0.clone(), queue_1.clone());
+        Duet {
+            machine_0: machine_0,
+            machine_1: machine_1,
+            send_count_1: 0,
+        }
+    }
+
+    pub fn run(&mut self) -> usize {
+        loop {
+            let result_0 = self.machine_0.execute();
+            let result_1 = self.machine_1.execute();
+            if let Ok((Instruction::Snd(_), _)) = result_1 {
+                self.send_count_1 += 1;
+            }
+            if result_0.is_err() && result_1.is_err() {
+                break;
+            }
+        }
+        self.send_count_1
     }
 }
--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,5 +1,5 @@
 extern crate day18;
-use day18::Machine;
+use day18::Duet;
 use day18::parser::parse_instruction;
 
 extern crate util;
@@ -15,6 +15,6 @@ fn main() {
             parse_instruction(line).expect("problem parsing line")
         })
         .collect::<Vec<_>>();
-    let mut machine = Machine::new(&instructions);
-    println!("first sound recovered: {:?}", machine.run());
+    let mut duet = Duet::new(&instructions);
+    println!("Machine 1 send count: {}", duet.run());
 }
